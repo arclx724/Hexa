ad26240fa9622f852500a1a86afae582331651c6
Yasir Aris M <git@yasir.id>
2026-02-12 11:27:55 +0700

    Style close inline buttons with danger + custom emoji

--- misskaty/core/misskaty_patch/__init__.py
+++ misskaty/core/misskaty_patch/__init__.py
@@ -1,6 +1,38 @@
 from . import bound, decorators, methods
+from pyrogram import enums
+from pyrogram.types import InlineKeyboardButton
+
 from misskaty.core.listener import setup_listener_patch
 
 
+_ORIG_IKB_INIT = InlineKeyboardButton.__init__
+_CLOSE_EMOJI_ID = 6037254263187443802
+
+
+def _patch_close_button_style():
+    if getattr(InlineKeyboardButton, "_misskaty_close_style_patched", False):
+        return
+
+    def _patched_init(self, *args, **kwargs):
+        _ORIG_IKB_INIT(self, *args, **kwargs)
+
+        callback_data = kwargs.get("callback_data")
+        if callback_data is None and len(args) > 1:
+            callback_data = args[1]
+
+        if isinstance(callback_data, str) and callback_data.startswith("close#"):
+            self.icon_custom_emoji_id = _CLOSE_EMOJI_ID
+            button_style = getattr(enums, "ButtonStyle", None)
+            self.style = (
+                button_style.DANGER
+                if button_style and hasattr(button_style, "DANGER")
+                else "DANGER"
+            )
+
+    InlineKeyboardButton.__init__ = _patched_init
+    InlineKeyboardButton._misskaty_close_style_patched = True
+
+
 def init_patch(client=None):
+    _patch_close_button_style()
     setup_listener_patch(client)
